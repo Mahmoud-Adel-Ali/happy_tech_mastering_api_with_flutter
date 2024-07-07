88de1427bf24106f345192c2a58d28833b2cd560
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-07 13:50:01 -0700

    replace all files

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:happy_tech_mastering_api_with_flutter/cubit/user_state.dart';
-import 'package:happy_tech_mastering_api_with_flutter/models/sign_in_model.dart';
-import 'package:happy_tech_mastering_api_with_flutter/repositories/user_repository.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit(this.userRepository) : super(UserInitial());
-  final UserRepository userRepository;
+  UserCubit() : super(UserInitial());
   //Sign in Form key
   GlobalKey<FormState> signInFormKey = GlobalKey();
   //Sign in email
@@ -28,47 +25,4 @@ class UserCubit extends Cubit<UserState> {
   TextEditingController signUpPassword = TextEditingController();
   //Sign up confirm password
   TextEditingController confirmPassword = TextEditingController();
-  SignInModel? user;
-
-  uploadProfilePic(XFile image) {
-    profilePic = image;
-    emit(UploadProfilePic());
-  }
-
-  signUp() async {
-    emit(SignUpLoading());
-    final response = await userRepository.signUp(
-      name: signUpName.text,
-      phone: signUpPhoneNumber.text,
-      email: signUpEmail.text,
-      password: signUpPassword.text,
-      confirmPassword: confirmPassword.text,
-      profilePic: profilePic!,
-    );
-    response.fold(
-      (errMessage) => emit(SignUpFailure(errMessage: errMessage)),
-      (signUpModel) => emit(SignUpSuccess(message: signUpModel.message)),
-    );
-  }
-
-  signIn() async {
-    emit(SignInLoading());
-    final response = await userRepository.signIn(
-      email: signInEmail.text,
-      password: signInPassword.text,
-    );
-    response.fold(
-      (errMessage) => emit(SignInFailure(errMessage: errMessage)),
-      (signInModel) => emit(SignInSuccess()),
-    );
-  }
-
-  getUserProfile() async {
-    emit(GetUserLoading());
-    final response = await userRepository.getUserProfile();
-    response.fold(
-      (errMessage) => emit(GetUserFailure(errMessage: errMessage)),
-      (user) => emit(GetUserSuccess(user: user)),
-    );
-  }
 }
